0e8639fc342000ecf58fb3c8e0828303a11fd0a7
Mr.Mao <951416545@qq.com>
2023-05-07 23:45:28 +0800

    fix(global): global type is empty

---
+++ src/global.ts
@@ -0,0 +1,52 @@
+import type { PixiSimpleRopeComponent } from './elements/simpleRope'
+import type { PixiSimplePlaneComponent } from './elements/simplePlane'
+import type { PixiNineSlicePlaneComponent } from './elements/nineSlicePlane'
+import type { PixiMeshComponent } from './elements/mesh'
+import type { PixiAnimatedSpriteComponent } from './elements/animatedSprite'
+import type { PixiTilingSpriteComponent } from './elements/tilingSprite'
+import type { PixiTextComponent } from './elements/text'
+import type { PixiGraphicsComponent } from './elements/graphics'
+import type { PixiContainerComponent } from './elements/container'
+import type { PixiSpriteComponent } from './elements/sprite'
+import type { PixiBitmapTextComponent } from './elements/bitmapText'
+
+import '@vue/runtime-core'
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    Container: PixiContainerComponent
+    PixiContainer: PixiContainerComponent
+
+    Sprite: PixiSpriteComponent
+    PixiSprite: PixiSpriteComponent
+
+    Graphics: PixiGraphicsComponent
+    PixiGraphics: PixiGraphicsComponent
+
+    Text: PixiTextComponent
+    PixiText: PixiTextComponent
+
+    BitmapText: PixiBitmapTextComponent
+    PixiBitmapText: PixiBitmapTextComponent
+
+    TilingSprite: PixiTilingSpriteComponent
+    PixiTilingSprite: PixiTilingSpriteComponent
+
+    AnimatedSprite: PixiAnimatedSpriteComponent
+    PixiAnimatedSprite: PixiAnimatedSpriteComponent
+
+    Mesh: PixiMeshComponent
+    PixiMesh: PixiMeshComponent
+
+    SimplePlane: PixiSimplePlaneComponent
+    PixiSimplePlane: PixiSimplePlaneComponent
+
+    NineSlicePlane: PixiNineSlicePlaneComponent
+    PixiNineSlicePlane: PixiNineSlicePlaneComponent
+
+    SimpleRope: PixiSimpleRopeComponent
+    PixiSimpleRope: PixiSimpleRopeComponent
+  }
+}
+
+export {}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable import/export */
 import type { App, Plugin } from 'vue-demi'
 import { Stage } from './components'
 
+export * from './global'
 export * from './components'
 export * from './renderer'
+export * from './composables'
 
 function install(app: App<any>) {
   app.component('Stage', Stage)
