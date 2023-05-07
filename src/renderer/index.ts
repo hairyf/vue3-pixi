215a312c819c61866a8c41c0d982d17e7b911aab
Mr.Mao <951416545@qq.com>
2023-05-07 21:51:07 +0800

    chore(renderer): simplify renderer

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -38,73 +38,67 @@ export function createPixiRenderer(options: CreatePixiRendererOptions = {}) {
   const { prefix = 'pixi' } = options
 
   return createRenderer<Container, Container>({
-    createElement: (name, _, __, vnodeProps) => {
+    createElement: (name, _, __, props) => {
       const Constructor = findConstructor(prefix, name)
 
-      if (Constructor) {
-        if (Constructor === Graphics)
-          return new Constructor(vnodeProps?.geometry)
-        if (Constructor === Text) {
-          return new Constructor(
-            vnodeProps?.text,
-            vnodeProps?.style,
-            vnodeProps?.canvas,
-          )
-        }
-        if (Constructor === BitmapText)
-          return new Constructor(vnodeProps?.text, vnodeProps?.style)
-
-        return new Constructor()
+      if (!Constructor) {
+        warn(`Unknown element ${name}`)
+        return new Container()
       }
 
-      warn(`Unknown element ${name}`)
-
-      return new Container()
+      switch (Constructor) {
+        case Graphics:
+          return new Constructor(props?.geometry)
+        case Text:
+          return new Constructor(props?.text, props?.style, props?.canvas)
+        case BitmapText:
+          return new Constructor(props?.text, props?.style)
+        default:
+          return new Constructor()
+      }
     },
 
     patchProp,
 
+    parentNode: node => node.parent,
+    createText: text => new Text(text),
+    createComment: () => new Container(),
+    remove: child => child.destroy(),
     insert: (child, parent, anchor) => {
       if (anchor)
         parent.addChildAt(child, parent.getChildIndex(anchor))
       else
         parent.addChild(child)
     },
-    remove: child => child.destroy(),
-    createText: text => new Text(text),
-    createComment: () => new Container(),
     nextSibling: (node) => {
       const index = node.parent.getChildIndex(node)
       if (node.parent.children.length <= index + 1)
         return null
-      return (node.parent.getChildAt(index + 1) as Container) ?? null
+      return node.parent.getChildAt(index + 1) as Container ?? null
     },
-    parentNode: node => node.parent,
     setElementText: (node, text) => {
-      if (node instanceof Text)
-        node.text = text
-      else
-        warn(`Text is only supported with ${prefix}-text element`)
+      node instanceof Text
+        ? node.text = text
+        : warn(`Text is only supported with ${prefix}-text element`)
     },
     setText: (node, text) => {
-      if (node instanceof Text)
-        node.text = text
+      node instanceof Text && (node.text = text)
     },
   })
 }
 
 function findConstructor(prefix: string, name: string) {
-  let ctor
+  let c
   if (name.startsWith(prefix)) {
     name = camelize(name)
-    ctor = elements[name.slice(prefix.length)]
+    c = elements[name.slice(prefix.length)]
   }
   else {
     name = camelize(name)
     name = name.charAt(0).toUpperCase() + name.slice(1)
-    ctor = elements[name]
+    c = elements[name]
   }
-  return ctor as undefined | (new (...args: any) => Container)
+  return c as undefined | (new (...args: any) => Container)
 }
 
 export const { createApp, render } = createPixiRenderer()
