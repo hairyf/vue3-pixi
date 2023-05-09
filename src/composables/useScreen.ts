2dd07ae30ad0fba7aa5eb8719f9449ad35a7e418
Mr.Mao <951416545@qq.com>
2023-05-10 03:01:02 +0800

    feat(composables): `useScreen`  app parameter

--- src/composables/useScreen.ts
+++ src/composables/useScreen.ts
@@ -1,14 +1,15 @@
 import type { Ref } from 'vue-demi'
 import { computed, unref } from 'vue-demi'
 
+import type { Application } from 'pixi.js'
 import { Rectangle } from 'pixi.js'
 import { useApplication } from './useApplication'
 
-export function useScreen(): Ref<Rectangle> {
-  const app = useApplication()
+export function useScreen(app?: Ref<Application | undefined>): Ref<Rectangle> {
+  const useApp = app || useApplication()
   const defaultRectangle = new Rectangle()
   const screenRef = computed(() =>
-    unref((app.value as any)?._v_screen),
+    unref((useApp.value as any)?._v_screen),
   )
   return computed(() => unref(screenRef) || defaultRectangle)
 }
