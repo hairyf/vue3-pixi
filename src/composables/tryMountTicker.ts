4ebdbca0c5599baa4c4b19ea802d80598c8ef44a
Mr.Mao <951416545@qq.com>
2023-05-09 23:09:05 +0800

    fix(tryMountTicker):The `tryMountTicker` parameter is passed as an app

--- src/composables/tryMountTicker.ts
+++ src/composables/tryMountTicker.ts
@@ -1,23 +1,21 @@
-import type { MaybeRef } from '@vueuse/core'
 import { tryOnBeforeUnmount, tryOnMounted } from '@vueuse/core'
-import type { TickerCallback } from 'pixi.js'
-import type { StageInst } from '../../dist'
+import type { Application, ICanvas, TickerCallback } from 'pixi.js'
+import type { Ref } from 'vue-demi'
 import { useApplication } from './useApplication'
 
-export function tryMountTicker(stageRef?: MaybeRef<StageInst>, fn?: TickerCallback<any>): () => void
+export function tryMountTicker(app?: Ref<Application<ICanvas> | undefined>, fn?: TickerCallback<any>): () => void
 export function tryMountTicker(fn?: TickerCallback<any>): () => void
 export function tryMountTicker(...args: any[]): any {
-  let stageRef: MaybeRef<StageInst> | undefined
   let fn: TickerCallback<any> = () => {}
+  let app = useApplication()
+
   if (typeof args[0] === 'function') {
     fn = args[0]
   }
   else {
-    stageRef = args[0]
+    app = args[0]
     fn = args[1]
   }
-
-  const app = useApplication(stageRef)
 
   function insert() {
     app.value?.ticker.add(fn)
