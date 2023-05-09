1102e8b47576c4fb58020c150e98b4b830434f10
Mr.Mao <951416545@qq.com>
2023-05-09 23:06:29 +0800

    fix(tryMountTicker): `tryMountTicker` cannot be used outside of setup

--- src/composables/tryMountTicker.ts
+++ src/composables/tryMountTicker.ts
@@ -1,9 +1,23 @@
+import type { MaybeRef } from '@vueuse/core'
 import { tryOnBeforeUnmount, tryOnMounted } from '@vueuse/core'
 import type { TickerCallback } from 'pixi.js'
+import type { StageInst } from '../../dist'
 import { useApplication } from './useApplication'
 
-export function tryMountTicker(fn: TickerCallback<any>) {
-  const app = useApplication()
+export function tryMountTicker(stageRef?: MaybeRef<StageInst>, fn?: TickerCallback<any>): () => void
+export function tryMountTicker(fn?: TickerCallback<any>): () => void
+export function tryMountTicker(...args: any[]): any {
+  let stageRef: MaybeRef<StageInst> | undefined
+  let fn: TickerCallback<any> = () => {}
+  if (typeof args[0] === 'function') {
+    fn = args[0]
+  }
+  else {
+    stageRef = args[0]
+    fn = args[1]
+  }
+
+  const app = useApplication(stageRef)
 
   function insert() {
     app.value?.ticker.add(fn)
