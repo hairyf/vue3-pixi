2bfb8734bb8ef9c30ca801c6d2fd522f2ba849c2
Mr.Mao <951416545@qq.com>
2023-05-09 20:26:23 +0800

    chore: release v0.5.0

--- src/composables/internal/index.ts
+++ src/composables/internal/index.ts
@@ -1,4 +1,5 @@
 import type { Application } from 'pixi.js'
+
 import type { InjectionKey, Ref } from 'vue-demi'
 
 export const applicationInjectionKey: InjectionKey<Ref<Application>> = Symbol('pixi_application')
