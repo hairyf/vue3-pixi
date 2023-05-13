740aa07c2bd803b9eb842fcdd23776b536b66a0d
Mr.Mao <951416545@qq.com>
2023-05-13 23:09:49 +0800

    feat(composables): replace tryMountTicker with public `Tickers` provided by pixi

--- src/composables/tryMountTicker.ts
+++ src/composables/tryMountTicker.ts
@@ -1,27 +1,13 @@
 import { tryOnBeforeUnmount, tryOnMounted } from '@vueuse/core'
-import type { Application, ICanvas, TickerCallback } from 'pixi.js'
-import type { Ref } from 'vue-demi'
-import { useApplication } from './useApplication'
+import type { TickerCallback } from 'pixi.js'
+import { Ticker } from 'pixi.js'
 
-export function tryMountTicker(app?: Ref<Application<ICanvas> | undefined>, fn?: TickerCallback<any>): () => void
-export function tryMountTicker(fn?: TickerCallback<any>): () => void
-export function tryMountTicker(...args: any[]): any {
-  let fn: TickerCallback<any> = () => {}
-  let app = useApplication()
-
-  if (typeof args[0] === 'function') {
-    fn = args[0]
-  }
-  else {
-    app = args[0]
-    fn = args[1]
-  }
-
+export function tryMountTicker(fn: TickerCallback<any>): any {
   function insert() {
-    app.value?.ticker.add(fn)
+    Ticker.shared.add(fn)
   }
   function remove() {
-    app.value?.ticker.remove(fn)
+    Ticker.shared.remove(fn)
   }
 
   tryOnMounted(insert)
