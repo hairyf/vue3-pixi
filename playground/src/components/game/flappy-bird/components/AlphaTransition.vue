344dc0b8789102f9218667cea520223f776f5de6
Mr.Mao <951416545@qq.com>
2023-05-14 22:14:38 +0800

    feat(example): flappy bird gameover page

---
+++ playground/src/components/game/flappy-bird/components/AlphaTransition.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { PTransition } from 'vue3-pixi-transition'
+</script>
+
+<template>
+  <PTransition
+    :duration="300"
+    :before-enter="{ alpha: 0 }"
+    :enter="{ alpha: 1 }"
+    :leave="{ alpha: 0 }"
+  >
+    <slot />
+  </PTransition>
+</template>
--- playground/src/components/game/flappy-bird/index.vue
+++ playground/src/components/game/flappy-bird/index.vue
@@ -6,25 +6,18 @@ import Background from './components/Background.vue'
 import Ground from './components/Ground.vue'
 import Score from './components/Score.vue'
 import Gameover from './components/Gameover.vue'
+import AlphaTransition from './components/AlphaTransition.vue'
 
 const gaming = ref(false)
 const gameover = ref(false)
-
-function wwww() {
-}
 </script>
 
 <template>
   <Main @click="gaming = true">
     <Background :blur="!gaming" />
     <Ground />
-    <PTransition
-      :duration="300"
-      :before-enter="{ alpha: 0 }"
-      :enter="{ alpha: 1 }"
-      :leave="{ alpha: 0 }"
-    >
-      <Gameover v-if="gaming" @restart="wwww" />
-    </PTransition>
+    <AlphaTransition>
+      <Gameover v-if="gaming" />
+    </AlphaTransition>
   </Main>
 </template>
