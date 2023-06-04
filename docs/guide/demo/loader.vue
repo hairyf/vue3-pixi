ca402a7d04b733839a0013bf557855c007829b75
Mr.Mao <951416545@qq.com>
2023-06-04 14:56:25 +0800

    feat: add loader demo to usage guide
    Added a new demo component to demonstrate the usage of loaders in Vue3-Pixi. The loader component uses the `Loader` class to load a list of images and show a loading screen until all images have finished loading. The demo component uses the `Application` component from Vue3-Pixi to create a PixiJS application and render the loaded images as sprites.

---
+++ docs/guide/demo/loader.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { Application, Loader } from 'vue3-pixi'
+
+const images = [
+  '/assets/food/lemonpie.png',
+  '/assets/food/strawberrycake.png',
+  '/assets/food/cheesecake.png',
+]
+</script>
+
+<template>
+  <Application :width="240" :height="240">
+    <Loader :resources="images">
+      <!-- loading state via #fallback slot -->
+      <template #fallback>
+        <text :anchor="0.5" :x="120" :y="120" :style="{ fill: 'white' }">
+          Loading...
+        </text>
+      </template>
+
+      <!-- component with nested async dependencies -->
+      <template #default="{ textures }">
+        <sprite
+          v-for="(image, k, i) in textures" :key="k"
+          :texture="image"
+          :anchor="0.5"
+          :x="70 + i * 50"
+          :y="120"
+          :scale="2"
+        />
+      </template>
+    </Loader>
+  </Application>
+</template>
--- docs/guide/usage.md
+++ docs/guide/usage.md
@@ -15,3 +15,11 @@ PixiJS will load the texture in the background and show it when it’s ready - s
 
 
 <demo src="./demo/sprite.vue" :app="false" />
+
+## Loaders
+
+If you have a bunch of images or other resources, you may wish to show a loading screen until all images have finished loading (rather than have them pop in one after another).
+
+> Note: You may want to enable network throttling in your browser dev tools to actually see the loading screen - these are small images!
+
+<demo src="./demo/loader.vue" :app="false" />
