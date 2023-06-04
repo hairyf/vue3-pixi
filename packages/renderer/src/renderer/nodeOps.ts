04699f4c3db699af634d28fdc39762f5b17714f4
Mr.Mao <951416545@qq.com>
2023-06-05 04:28:33 +0800

    feat(nodeOps): improve createElement implementation and filter handling
    Refactored createElement to use the new renderers registry and added handling to set a flag on Filters for identification.

--- packages/renderer/src/renderer/nodeOps.ts
+++ packages/renderer/src/renderer/nodeOps.ts
@@ -1,30 +1,39 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { BitmapText, Container, DisplayObject, Filter, Text, Texture } from 'pixi.js'
-import { markRaw, warn } from 'vue-demi'
-import { isCustomFilter, isOn } from '../utils'
-import {
-  Empty,
-  createPixiElement,
-  insertContainer,
-  insertFilter,
-  nextSiblingContainer,
-  nextSiblingFilter,
-} from './ops'
+import { camelize, markRaw, warn } from 'vue-demi'
+import { isOn } from '../utils'
+import { Empty, insertContainer, insertFilter, nextSiblingContainer, nextSiblingFilter, renderers } from './internal'
 
-export function createElement(prefix: string, name: string, props: any) {
-  const element = isCustomFilter(prefix, name)
-    ? props?.is?.(props)
-    : createPixiElement(prefix, name, props)
+export function createElement(prefix: string, name: string, _?: boolean, _1?: string, props?: any): any {
+  let is
+  if (name.startsWith(prefix)) {
+    name = camelize(name)
+    is = renderers[name.slice(prefix.length)].createElement
+  }
+  else {
+    name = camelize(name)
+    name = name.charAt(0).toUpperCase() + name.slice(1)
+    is = renderers[name].createElement
+  }
+  if (!is) {
+    warn(`Unknown element ${name}`)
+    is = () => new Container()
+  }
+  const element = is(props ?? {})
   if (element instanceof DisplayObject)
   // @ts-expect-error
     isOn(props) && element.eventMode === 'auto' && (element.eventMode = 'static')
+
+  if (element instanceof Filter)
+    element._vp_filter = true
+
   markRaw(element)
+
   return element
 }
 
 export function parentNode(node: any) {
-  node && (node.__parent ??= node.parent)
-  return node?.__parent || node?.parent
+  return node?.parent
 }
 
 export function createText(text: string) {
@@ -41,22 +50,21 @@ export function remove(node: Container) {
 }
 
 export function insert(child: Container, parent: Container, anchor?: Container | null) {
-  if (child instanceof Filter)
-    return insertFilter(child, parent, anchor)
-  if (parent instanceof Container && child instanceof DisplayObject)
+  if (Reflect.get(child, '_is_filter'))
+    return insertFilter(child as unknown as Filter, parent, anchor)
+  else
     return insertContainer(child, parent, anchor)
 }
 
-export function nextSibling(node: Container) {
-  if (node instanceof Filter)
-    return nextSiblingFilter(node)
-  if (node instanceof DisplayObject)
+export function nextSibling(node: Container): any {
+  if (Reflect.get(node, '_is_filter'))
+    return nextSiblingFilter(node as unknown as Filter)
+  else
     return nextSiblingContainer(node)
 }
 
 export function setText(prefix: string, node: Container, text: string) {
   text = text.replace(/\\n/g, '\n')
-
   node instanceof Text || node instanceof BitmapText
     ? node.text = text.trim()
     : warn(`Text is only supported with ${prefix}-text element`)
