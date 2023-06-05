6ea9653e08b5272cb247795ba8c828ec5505d525
mrmao <wwu710632@gmail.com>
2023-06-05 17:25:16 +0800

    feat(docs): update social link to new repository
    Update the GitHub social link in the documentation to point to the new repository.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/hairyf/vue3-pixi' },
     ],
   },
 
