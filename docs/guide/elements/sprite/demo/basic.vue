45d0b58061d3031d78b7e6078d8e90aaca31952e
mrmao <wwu710632@gmail.com>
2023-06-07 12:51:13 +0800

    feat(sprite): improve demo appearance
    Simplify sprite demo code and adjust anchor and scale properties for better appearance.

--- docs/guide/elements/sprite/demo/basic.vue
+++ docs/guide/elements/sprite/demo/basic.vue
@@ -1,9 +1,7 @@
 <template>
   <sprite
     texture="/assets/adventurer.png"
-    :anchor="0.5"
-    :scale="2"
-    :x="120"
-    :y="120"
+    :anchor="0.5" :scale="1.2"
+    :x="120" :y="120"
   />
 </template>
